ac6e9f9a7b46d43f490931f8db93c37af9b7abdb
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-12-02 19:02:50 +0200

    add changes

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './js/swiper';
+import './js/sound';
+import './js/predloading';
+import './js/gomerBtn';
---
+++ src/js/modal.js
@@ -0,0 +1,48 @@
+(() => {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const openMenuIcon = document.querySelector('.site-nav__icon');
+    const header = document.querySelector('.header');
+
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      openMenuIcon.classList.toggle('open-mode');
+      header.classList.toggle('header--lightgrey-color');
+      mobileMenu.classList.toggle('is-open');
+
+      if (!isMenuOpen) {
+        document.body.style.overflow = 'hidden';
+        const mobileMenu = document.querySelector('.js-menu-container');
+        const clickLink = document.querySelectorAll('.modal-nav__link');
+        clickLink.forEach(el => {
+          el.addEventListener('click', () => {
+            openMenuIcon.classList.remove('open-mode');
+            header.classList.remove('header--lightgrey-color');
+            mobileMenu.classList.remove('is-open');
+            document.body.style.overflow = 'visible';
+          });
+        });
+      } else {
+        document.body.style.overflow = 'visible';
+      }
+
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
